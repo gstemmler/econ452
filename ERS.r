fe5ff1793b414107092940e16ebf9544dbaa1548
abluebb <57112162+abluebb@users.noreply.github.com>
2019-12-08 02:53:21 -0800

    Update ERS.r

--- ERS.r
+++ ERS.r
@@ -1,12 +1,28 @@
 #Author: ECON452 - team Charlie
 #Date: Fall 2019
 #Subject: ERS
+
+# LOAD PACKAGES
+library(xts)
+library(zoo)
+
+# -------------------------------------------------------------------------------------------
+# LOAD and CLEAN DATA
 setwd("C:/Users/6o8an/OneDrive/Documents/ECON452")
+
 daily <- read.csv("ers/daily.csv", header = TRUE, stringsAsFactors = FALSE)
+daily$date <- as.Date(daily$date, "%Y-%m-%d")
+
 weekly <- read.csv("ers/weekly.csv", header = TRUE, stringsAsFactors = FALSE)
+colnames(weekly)[2] <- "Mortage_Rate_30Year"
+weekly$Period <- as.Date(weekly$Period, "%Y-%m-%d")
+
 monthly <- read.csv("ers/monthly.csv", header = TRUE, stringsAsFactors = FALSE)
 monthly <- monthly[1:238,1:7]
-colnames(weekly)[2] <- "Mortage_Rate_30Year"
+monthly$date <- as.Date(monthly$date , "%Y-%m-%d")
+
+# -------------------------------------------------------------------------------------------
+# CONVERT w/ TS FUNCTION
 
 y.ts<- ts(daily$WILLREITIND, start = c(2000,1,3), frequency = 252.75)
 S5UTIL.ts <- ts(daily$S5UTIL, start = c(2000,1,3), frequency = 252.75)
@@ -22,3 +38,7 @@ PPI.Lumber.ts <- ts(monthly$PPI_Lumber_Wood, start = c(2000,1),frequency = 12)
 PPI.Rubber.ts <- ts(monthly$PPI_rubber_plastic, start = c(2000,1),frequency = 12)
 PPI.Tranportation.ts <- ts(monthly$PPI_Transportation_Equipment, start = c(2000,1),frequency = 12)
 PPI.Machinery.ts <- ts(monthly$PPI_Machinery_Equipment, start = c(2000,1),frequency = 12)
+
+
+MortgageRate.xts <- xts(weekly$Mortage_Rate_30Year,order.by = weekly$Period)
+
